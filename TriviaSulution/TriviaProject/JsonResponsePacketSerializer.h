7d93ede6eb325d469d4238eb0df6a8d0da4ae9cd
Assaf Kabesa <asafka73@gmail.com>
2021-04-21 19:21:56 +0300

    added structs for error, login and signup responses.

--- TriviaSulution/TriviaProject/JsonResponsePacketSerializer.h
+++ TriviaSulution/TriviaProject/JsonResponsePacketSerializer.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <iostream>
+struct LoginResponse
+{
+	unsigned int status;
+};
+
+struct SignupResponse
+{
+	unsigned int status;
+};
+
+struct ErrorResponse
+{
+	std::string message;
+};
+
 class JsonResponsePacketSerializer
 {
+
 };
 
