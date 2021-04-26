758083f7476c223ec5b6543e8427faf649496b5c
Assaf Kabesa <asafka73@gmail.com>
2021-04-26 19:19:32 +0300

    finished declaring on function in SqliteDataBase class.

--- TriviaSulution/TriviaProject/SqliteDataBase.cpp
+++ TriviaSulution/TriviaProject/SqliteDataBase.cpp
@@ -1,1 +1,15 @@
 #include "SqliteDataBase.h"
+
+bool SqliteDataBase::doesUserExist(std::string username)
+{
+	return false;
+}
+
+void SqliteDataBase::addNewUser(std::string username, std::string password, std::string mail)
+{
+}
+
+bool SqliteDataBase::doesPasswordMatch(std::string username, std::string password)
+{
+	return false;
+}
--- TriviaSulution/TriviaProject/SqliteDataBase.h
+++ TriviaSulution/TriviaProject/SqliteDataBase.h
@@ -3,5 +3,9 @@
 
 class SqliteDataBase : public IDatabase
 {
+public:
+	virtual bool doesUserExist(std::string username);
+	virtual bool doesPasswordMatch(std::string username, std::string password);
+	virtual void addNewUser(std::string username, std::string password, std::string mail);
 };
 
