4d43d72d7df65e8a9b3165b069b72b2c99b63364
leon sonnenschein <369leon@gmail.com>
2021-05-30 14:29:27 +0000

    Revert "god doesn't exists."
    This reverts commit bc4aa27d748c9e17c4406f1225616a71851054f3

--- GUI/TriviaGUI/TriviaGUI/CreateMenu.xaml.cs
+++ GUI/TriviaGUI/TriviaGUI/CreateMenu.xaml.cs
@@ -12,10 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
-using System.Net.Sockets;
-using System.Threading;
-using Newtonsoft.Json;
-using ServerFunctions;
 
 namespace TriviaGUI
 {
@@ -39,40 +35,6 @@ namespace TriviaGUI
             MainMenu menu = new MainMenu();
             menu.Show();
             this.Close();
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            TcpClient serverConnection = (TcpClient)App.Current.Properties["server"];
-            Dictionary<string, object> RoomToBeCreatedDitalis = new Dictionary<string, object>();
-            RoomToBeCreatedDitalis.Add("roomName", this.roomName_TB.Text);
-            RoomToBeCreatedDitalis.Add("maximumUsers", this.MaximumUsers_TB.Text);
-            RoomToBeCreatedDitalis.Add("questionCount", this.QuestionCount_TB.Text);
-            RoomToBeCreatedDitalis.Add("timeToAnswer", this.AnswerTimeOut_TB.Text);
-            string json_parsed = JsonConvert.SerializeObject(RoomToBeCreatedDitalis); // serializing data
-            byte[] json_byted = System.Text.Encoding.ASCII.GetBytes(json_parsed); // encoding json
-            byte[] data_encoded = ServerFunctions.ServerFunctions.getCompleteMsg(7, json_byted); // adding size and code for protocal
-            serverConnection.GetStream().Write(data_encoded, 0, 1000);
-
-            while (serverConnection.Available == 0) ; // wait until a new message arrived from the server
-            byte[] serverOutput = ServerFunctions.ServerFunctions.ReadServerMessage(serverConnection);
-            Newtonsoft.Json.Linq.JObject jsonReturned = ServerFunctions.ServerFunctions.diserallizeResponse(serverOutput); // diserallizing json from server
-
-            try
-            {
-                if (jsonReturned["status"].ToString() == "1")
-                {
-                    this.IsCreated_TB.Text = "The Room was successfully created.";
-                }
-                else
-                {
-                    this.IsCreated_TB.Text = "The Room wasn't created.";
-                }
-            }
-            catch (Exception ex)
-            {
-                this.IsCreated_TB.Text = "The Room wasn't created.";
-            }
         }
     }
 }
