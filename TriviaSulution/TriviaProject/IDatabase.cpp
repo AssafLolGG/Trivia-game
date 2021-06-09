01da6b514601096e43ea4fa7b7c3ded22493ba74
Assaf Kabesa <asafka73@gmail.com>
2021-06-09 15:38:27 +0300

    added function to get specific number of questions from db.

--- TriviaSulution/TriviaProject/IDatabase.cpp
+++ TriviaSulution/TriviaProject/IDatabase.cpp
@@ -1,1 +1,12 @@
 #include "IDatabase.h"
+
+std::vector<Question> IDatabase::getQuestions(int num_of_questions)
+{
+	std::vector<Question> questions_vec;
+	std::vector<Question> all_questions_vec = this->getAllQuestions();
+	for (int i = 0; i < all_questions_vec.size() && i < num_of_questions; i++)
+	{
+		questions_vec.push_back(all_questions_vec[i]);
+	}
+	return questions_vec;
+}
